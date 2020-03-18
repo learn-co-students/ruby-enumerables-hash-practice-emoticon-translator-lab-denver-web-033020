d99e78c802646e074942230933fb8fdfad07af43
Kathryn Clark <kathrynlael@gmail.com>
2020-03-18 20:09:41 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,32 @@
+require 'yaml'
+require 'pry'
+#pp emoticons_file
+
 # require modules here
 
-def load_library
-  # code goes here
+def load_library(filepath)
+  emoticons_file = YAML.load_file("./lib/emoticons.yml")
+  #emoticons_file[get_meaning] = get_emoticon
+ return emoticons_file
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(filepath, emoticon)
+  emoticons_file = load_library(filepath)
+  #binding.pry
+  if emoticons_file['get_emoticon'][emoticon]
+    return emoticons_file['get_emoticon'][emoticon]
+  else 
+     return "Sorry, that emoticon was not found"
+  end
 end
 
-def get_english_meaning
-  # code goes here
+
+
+def get_english_meaning(filepath, emoticon)
+  emoticons_file = load_library(filepath)
+   if emoticons_file['get_meaning'][emoticon]
+    return emoticons_file['get_meaning'][emoticon]
+  else 
+     return "Sorry, that emoticon was not found"
+  end
 end
